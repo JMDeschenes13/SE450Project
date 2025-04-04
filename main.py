0eff12eec42d7d0471029ebe05be2756d66b6182
JMDeschenes13 <103342404+JMDeschenes13@users.noreply.github.com>
2025-04-04 11:05:56 -0500

    Created file output functionality

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ with open("mal-p2.exe", "rb") as file:
 strings = binary2strings.extract_all_strings(binary_data)
 
 for string in strings:
-    print(string)
+    print(string[0])
+
+
+with open("StringsOutput.txt", "w", encoding="utf-8") as file:
+    for string in strings:
+        file.write(f"{string[0]}\n")
